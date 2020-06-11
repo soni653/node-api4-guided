c5e092d2464a363c377907d4bfbe1542079df97d
soni653 <dave.family653@outlook.com>
2020-06-11 15:21:59 -0400

    connecting to heroku

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.status(200).json({
-		message: "Welcome",
-	})
-})
+  res.status(200).json({
+    message: `Welcome ${process.env.COHORT}`,
+    fact: process.env.FUN_FACT || "I am not fun",
+  });
+});
 
-module.exports = router
+module.exports = router;
